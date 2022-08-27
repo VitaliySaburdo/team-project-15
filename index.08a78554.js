baa2d9ac58138b7d50cf2b2ee4b75aa023ec5565
VitaliySaburdo <107457394+VitaliySaburdo@users.noreply.github.com>
2022-08-27 06:10:10 +0000

    Deploying to gh-pages from @ VitaliySaburdo/team-project-15@c297ba235aaa6d3fddb3951fdb5ef9f45ed0ab0d 🚀

---
+++ index.08a78554.js
@@ -0,0 +1,2 @@
+let e=0;const t=document.querySelector(".slider__line");document.querySelector(".swiper-button-next").addEventListener("click",(function(){e+=528,e>1056&&(e=0),t.style.left=-e+"px"})),document.querySelector(".swiper-button-prev").addEventListener("click",(function(){e-=528,e<0&&(e=1056),t.style.left=-e+"px"}));
+//# sourceMappingURL=index.08a78554.js.map
---
+++ index.202ca351.js
@@ -0,0 +1,2 @@
+!function(){var e=0,t=document.querySelector(".slider__line");document.querySelector(".swiper-button-next").addEventListener("click",(function(){(e+=528)>1056&&(e=0),t.style.left=-e+"px"})),document.querySelector(".swiper-button-prev").addEventListener("click",(function(){(e-=528)<0&&(e=1056),t.style.left=-e+"px"}))}();
+//# sourceMappingURL=index.202ca351.js.map
