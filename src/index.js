6ac9c2b5e997f409097829f63114b9b42ff8aac4
Vitaliy Saburdo <vsaburdo@gmail.com>
2022-08-28 21:38:09 +0300

    mobil scroll

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./js/mobil-menu";
 import "./js/slider";
 import "./js/modal-window";
-import "./js/slowscroll";
+import "./js/slowscroll_header";
 import "./js/slowscroll_footer";
+import "./js/slowscroll_mobil";
