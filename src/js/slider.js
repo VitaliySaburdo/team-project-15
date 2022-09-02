e7f327467153e0b9edcd992032b2b76c958c2c6e
VitaliySaburdo <vsaburdo@gmail.com>
2022-09-02 20:56:49 +0300

    fix bug

--- src/js/slider.js
+++ src/js/slider.js
@@ -14,41 +14,7 @@ document.querySelector('.swiper-button-prev').addEventListener('click', function
     sliderLine.style.left = -offset + 'px';
 });
 
-// const images = document.querySelectorAll('.hero__slider--container .slider__line img');
-// const sliderLine = document.querySelector('.slider__line');
-// let count = 0;
-// let width;
 
-// function init() {
-//     console.log('resize');
-//     width = querySelector('.hero__slider--container').offsetWidth;
-//     sliderLine.style.width = width * images.lenght + 'px';
-//     images.forEach( item => {
-//         item.style.width = width + 'px';
-//         item.style.height = 'auto';
-//     });
-//     rollSlider();
-// }
-
-// window.querySelector('resize', init);
-// init();
-
-// document.querySelector('.swiper-button-prev').addEventListener('click', function () {
-//     count--;
-//     if (count < 0) { count = images.length - 1; }
-//     rollSlider();
-    
-// });
-
-// document.querySelector('.swiper-button-next').addEventListener('click', function () {
-//     count--;
-//     if (count >= images.length) { count = 0; }
-//     rollSlider();
-    
-// });
-// function rollSlider() {
-//     sliderLine.style.transform = 'translate(-' + count * width + 'px)';
-// }
 
 
 
