fd6a6744b9505cc7a956e730ef5603b4d181c8f3
VitaliySaburdo <vsaburdo@gmail.com>
2022-09-03 14:16:12 +0300

    add swiper setting

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,13 +1,27 @@
-const swiper = new Swiper('.hero__slider--container',{
+const swiper = new Swiper('.hero__slider--container', {
+  
+  // Кнопки навигации
     navigation: {
     nextEl: '.slider__btn--left',
     prevEl: '.slider__btn--right',
   },
 
+  // Пагинация
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
+
+  // Автоперелистывание
+  autoplay: {
+    // Пауза между прокруткой
+    delay: 3000,
+    // Отключение послеручного переключения
+    disableOnInteraction: false
+  }, 
+  // Скорость прокрутки
+  speed: 800, 
+
 });
 
 
