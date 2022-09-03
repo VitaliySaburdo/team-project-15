04d3c43fefc1f4d759c93f5a1b90c60269cb7b17
VitaliySaburdo <vsaburdo@gmail.com>
2022-09-03 12:42:52 +0300

    add swiper-slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,7 +1,12 @@
 const swiper = new Swiper('.hero__slider--container',{
     navigation: {
-    nextEl: '.slider__btn--right',
-    prevEl: '.slider__btn--left',
+    nextEl: '.slider__btn--left',
+    prevEl: '.slider__btn--right',
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
   },
 });
 
