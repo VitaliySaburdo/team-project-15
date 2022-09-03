6d9b11f024f1c8c23bf48131c830d55a94a2004d
VitaliySaburdo <vsaburdo@gmail.com>
2022-09-03 11:38:53 +0300

    add slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,17 +1,8 @@
-let offset = 0;
-const sliderLine = document.querySelector('.slider__line');
-
-document.querySelector('.swiper-button-next').addEventListener('click', function () {
-    offset = offset + 528;
-    if (offset > 1056) { offset = 0; }
-    sliderLine.style.left = -offset + 'px';
-});
-
-document.querySelector('.swiper-button-prev').addEventListener('click', function () {
-    offset = offset - 528;
-    if (offset < 0)
-    { offset = 1056; }
-    sliderLine.style.left = -offset + 'px';
+const swiper = new Swiper('.hero__slider--container',{
+    navigation: {
+    nextEl: '.slider__btn--right',
+    prevEl: '.slider__btn--left',
+  },
 });
 
 
