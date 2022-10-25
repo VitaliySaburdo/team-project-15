2d5aafcb4b4eddd3e151c2280177ea4774fb5183
VitaliySaburdo <107457394+VitaliySaburdo@users.noreply.github.com>
2022-10-25 11:25:14 +0300

    Update modal-window.js

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -1,16 +1,32 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelectorAll("[data-modal-open]"),
+    openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    backdrop: document.querySelector("[data-modal]"),
   };
-  
-  refs.openModalBtn.forEach(btn => {btn.addEventListener("click", toggleModal)
 
-});
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-  function toggleModal() {
-    document.body.classList.toggle("modal-open")
-    refs.modal.classList.toggle("is-hidden");
+  refs.openModalBtn.addEventListener("click", onOpenModal);
+  refs.closeModalBtn.addEventListener("click", onCloseModal);
+  refs.backdrop.addEventListener("click", onBackdropClick);
+
+  
+
+  function onOpenModal() {
+    refs.backdrop.classList.remove("is-hidden");
+    window.addEventListener('keydown', onEscPressKey)
+  }
+  function onCloseModal() {
+    refs.backdrop.classList.add("is-hidden");
+    window.removeEventListener('keydown',onEscPressKey)
+  }
+  function onBackdropClick(evt) {
+    if (evt.currentTarget === evt.target) {
+    onCloseModal()
+    }
+  }
+  function onEscPressKey(evt) {
+    if (evt.code === 'Escape') {
+      onCloseModal()
+    }
   }
 })();
